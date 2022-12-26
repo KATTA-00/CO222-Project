d8d8ffd584cff8830bb21fd7b0896480819312ce
KATTA-00 <kanishkagunawarthana@gmail.com>
2022-12-26 18:01:00 +0530

    get the spaces cord

--- test/test3.c
+++ test/test3.c
@@ -18,7 +18,18 @@ int checkCell(int x, int y, char arr[4][4])
     return 0;
 }
 
-void updateHorizon(int x, int y, char arr[4][4], int lens[])
+int isInArr(int n, int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == n)
+            return 1;
+    }
+
+    return 0;
+}
+
+void updateHorizon(int x, int y, char arr[4][4], int lens[], int wordLens[], int words)
 {
     int tempX = x;
     int i = 0;
@@ -45,8 +56,61 @@ void updateHorizon(int x, int y, char arr[4][4], int lens[])
         }
     }
 
-    lenSpace[spaceCount] = i;
-    spaceCount++;
+    if (isInArr(i, wordLens, words))
+    {
+        lenSpace[spaceCount] = i;
+        spaceCount++;
+    }
+    else
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            spaces[spaceCount][i][0] = -1;
+            spaces[spaceCount][i][1] = -1;
+        }
+    }
+}
+
+void updateVertical(int x, int y, char arr[4][4], int lens[], int wordLens[], int words)
+{
+    int tempY = y;
+    int i = 0;
+    while (tempY < 4)
+    {
+        if (checkCell(x, tempY, arr))
+        {
+            spaces[spaceCount][i][0] = x;
+            spaces[spaceCount][i][1] = tempY;
+            cordCheck[x][tempY].horiCheck = 1;
+            i++;
+        }
+        tempY++;
+    }
+    while (y >= 0)
+    {
+        y--;
+        if (checkCell(x, y, arr))
+        {
+            spaces[spaceCount][i][0] = x;
+            spaces[spaceCount][i][1] = y;
+            cordCheck[x][y].horiCheck = 1;
+            i++;
+        }
+    }
+
+    if (isInArr(i, wordLens, words))
+    {
+        lenSpace[spaceCount] = i;
+        spaceCount++;
+    }
+    else
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            spaces[spaceCount][i][0] = -1;
+            spaces[spaceCount][i][1] = -1;
+        }
+    }
 }
 
 int main()
@@ -54,8 +118,11 @@ int main()
 
     char arr[4][4] = {{'#', '#', '#', '#'},
                       {'#', '#', '*', '#'},
-                      {'#', '*', '*', '*'},
+                      {'*', '*', '*', '*'},
                       {'#', '#', '*', '#'}};
+
+    int wordLengths[] = {4, 2, 3};
+    int words = 3;
 
     for (int i = 0; i < 10; i++)
     {
@@ -75,14 +142,15 @@ int main()
         }
     }
 
-    updateHorizon(3, 2, arr, 1);
-    updateHorizon(2, 1, arr, 2);
+    updateHorizon(1, 2, arr, wordLengths, wordLengths, words);
+    updateHorizon(2, 1, arr, wordLengths, wordLengths, words);
+    updateVertical(2, 0, arr, wordLengths, wordLengths, words);
 
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 4; j++)
         {
-            printf("(%d %d)/%d  ", spaces[i][j][0], spaces[i][j][1]);
+            printf("(%d %d)  ", spaces[i][j][0], spaces[i][j][1]);
         }
         printf(" - %d \n", lenSpace[i]);
     }
